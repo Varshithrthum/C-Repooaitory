8a00f45a0725abf0dea58d69ec6779fbcde275a4
varshith <varshith2620@gmail.com>
2024-07-25 11:17:28 +0100

    Add project files.

---
+++ C#/FileName.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_
+{
+    internal class FileName
+    {
+
+        static void Main()
+        {
+            Console.WriteLine("Enter your First name1");
+            string Firstname = Console.ReadLine();
+            Console.WriteLine("Enter your Last name1");
+            string Lastname = Console.ReadLine();
+
+            Console.WriteLine("Hello {0} {1}", Firstname, Lastname);
+
+
+        }
+    }
+}
---
+++ C#/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+//All about ReadLine() and Writeline()
+namespace MyApplication
+{
+    class program
+    {
+        
+            static void Main()
+            {
+                Console.WriteLine("Enter your First name");
+                string Firstname = Console.ReadLine();
+                Console.WriteLine("Enter your Last name");
+                string Lastname = Console.ReadLine();
+
+                Console.WriteLine("Hello {0} {1}", Firstname, Lastname);
+
+            
+        }
+            
+    }
+}
+
